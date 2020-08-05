8fb040bb319add3af7921efd86699af89db5397c
SUSHMITHA.SK <51911682+sushmitha1109@users.noreply.github.com>
2020-08-05 06:10:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var box1;
+
+function setup() {
+ var canvas= createCanvas(400,400);
+ engine = Engine.create();
+  box1= new Box();
+ 
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+  box1.display();
+ 
+  
+}
